4250b1967b087558a1507c34c5fcd7c834f88bd1
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-08 20:48:29 +0530

    age

--- 5jan.py
+++ 5jan.py
@@ -1,1 +1,12 @@
+Write a Java program to store your age and print it
+
+import java.util.Scanner;
+public class age{
+   public static void main(String[]args){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter your age");
+      int age=sc.nextInt();
+      System.out.println("your age is "+ age);
+  }
+}
 
