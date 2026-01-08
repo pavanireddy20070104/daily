778a460886bb00df237166794bbfbe83624c4587
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-08 20:55:20 +0530

    calculate sum of numbers

--- 6jan.py
+++ 6jan.py
@@ -1,1 +1,15 @@
+Write a program to store two integers and print their sum.
 
+import java.util.Scanner;
+public class age{
+  public static void main(String[]args){
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter num1 value:");
+  int num1=sc.nextInt();
+  System.out.println("enter num2 value:");
+  int num2=sc.nextInt();
+  int sum=num1+num2;
+  System.out.println("sum of numbers is:"+sum);
+  sc.close();
+   }
+}
