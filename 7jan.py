5cfa506cb7238349e37e7512d5f8904b750a1325
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-08 21:07:35 +0530

    Correct grade
    Fix typos in print statements and improve formatting.

--- 7jan.py
+++ 7jan.py
@@ -1,1 +1,24 @@
+//Write a program to calculate grade using marks:
+≥90 → A
+≥75 → B
+≥50 → C
+Else → Fail
 
+import java.util.Scanner;
+public class age{
+   public static void main(String[]args){
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter your marks:");
+  double marks=sc.nextDouble();
+  if (marks>=90){
+    System.out.println("grade A");
+   } else if(marks >=75){
+     System.out.println("grade B");
+  } else if (marks >=50){
+     System.out.pritnln("grade C");
+  }else{
+    System.out.printlm("FAIL");
+  }
+sc.close();
+}
+}
