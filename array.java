c07a55f797f4b6561eb193ad3c124f5d8a7f45e2
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-20 21:23:46 +0530

    Printing numbers using array

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[]args)
+    {
+        //to print the numbers stored in array
+        int[]array= {10,20,30,40};
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+
+        }
+        }
+
+    }
