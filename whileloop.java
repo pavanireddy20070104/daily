a6a6077c4e93360576bd84097d8367e8295154fa
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-22 14:12:10 +0530

    while loop basic examples

---
+++ whileloop.java
@@ -0,0 +1,23 @@
+//Print 1 to 5
+
+public class num{
+  public static void main(String[]args){
+    int i=0;
+      while(i <= 5){
+      System.out.println(i);
+      i++;
+    }
+  }
+}
+
+
+//Print numbers from 5 to 1 (reverse)
+public class Hello{
+  public static void main(String[]args){
+   int i =5;
+    while(i >=1){
+      System.out.println(i);
+     i--;
+    }
+  }
+}
