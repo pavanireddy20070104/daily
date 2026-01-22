61c62fd7e17f4e632467575d3f45baf19db9d322
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-22 08:13:55 +0530

    for loop examples

---
+++ forloop.java
@@ -0,0 +1,19 @@
+//Print 1 to 5
+public class Hello{
+  public static void main(String[]args){
+  for(int i=1;i <= 5; i++){
+    System.out.println(i);
+    }
+  }
+}
+
+//print 5 to 0 (reverse order)
+public class Hello{
+  public static void main(String[]args){
+  for(int i=5;i >0; i--){
+    System.out.println(i);
+   }
+  }
+}
+
+
