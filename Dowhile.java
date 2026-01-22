bbf646d04940ba4b1993c5de9ffacb9379028d50
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-22 14:28:07 +0530

    basic do while example to print numbers

---
+++ Dowhile.java
@@ -0,0 +1,14 @@
+//print 1 to 10 numbers
+
+public class Hello{
+  public static void main(String[]args){
+   int i =1;
+    do{
+     System.out.println(i);
+      i++;
+     } 
+     while(i <=10);
+    
+  }
+
+}
