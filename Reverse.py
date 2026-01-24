e3e94c53c66e97e49d34ed7b2263f6774ce5389e
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-24 20:48:11 +0530

    printing reverse numbers using while loop

---
+++ Reverse.py
@@ -0,0 +1,10 @@
+//Print numbers from 5 to 1 (reverse)
+public class Hello{
+  public static void main(String[]args){
+   int i =5;
+    while(i >=1){
+      System.out.println(i);
+     i--;
+    }
+  }
+}
