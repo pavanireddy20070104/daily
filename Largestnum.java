404ef99a81d194566cd8a240668925fc3b108a71
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-24 20:44:45 +0530

    largest number of two numbers

---
+++ Largestnum.java
@@ -0,0 +1,19 @@
+//Write a Java program to find the largest of two numbers.
+import java.util.Scanner;
+public class numbers{
+ public static void main(String[]args){
+ Scanner sc=new Scanner(System.in);
+   System.out.println("enter the num1:");
+   int num1=sc.nextInt();
+   System.out.println("enter the num2:");
+   int num2=sc.nextInt();
+   if (num1>num2){
+    System.out.println(num1);
+   } else if(num2>num1){
+    System.out.println(num2);
+   }else{
+    System.out.println("both are equal");
+   }
+   sc.close();
+ }
+}
