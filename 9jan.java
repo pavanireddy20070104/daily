7c7b6d74cea81b3c21a36f9539d773b33945cca0
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-24 20:43:28 +0530

    even or add

--- 9jan.java
+++ 9jan.java
@@ -1,5 +1,4 @@
 //Write a Java program to check whether a number is even or odd.
-
 import java.util.Scanner;
 public class number{
   public static void main(String[]args){
@@ -10,45 +9,6 @@ public class number{
       System.out.println("entered number is even");
     } else{
       System.out.println("entered number is odd");
-    }
-    sc.close();
-  }
-}
-
-//Write a Java program to find the largest of two numbers.
-
-import java.util.Scanner;
-public class numbers{
- public static void main(String[]args){
- Scanner sc=new Scanner(System.in);
-   System.out.println("enter the num1:");
-   int num1=sc.nextInt();
-   System.out.println("enter the num2:");
-   int num2=sc.nextInt();
-   if (num1>num2){
-    System.out.println(num1);
-   } else if(num2>num1){
-    System.out.println(num2);
-   }else{
-    System.out.println("both are equal");
-   }
-   sc.close();
- }
-}
-
-//Write a Java program to check whether a number is positive, negative, or zero.
-import java.util.Scanner;
-public class number{
-  public static void main(String[]args){
-  Scanner sc=new Scanner(System.in);
-    System.out.println("enter the number:");
-    int num=sc.nextInt();
-    if (num>0){
-     System.out.println("entered  number is positive");
-    } else if(num<0){
-      System.out.println("entered  number is negative");
-    }else{
-      System.out.println("number is zero:");
     }
     sc.close();
   }
